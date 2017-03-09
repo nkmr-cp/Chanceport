396985db40d56d456bb28bd4169ddbb1d949201f
中村 彰男 <studentsandsociety@gmail.com>
2017-03-09 18:17:44 +0900

    modified production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,16 +79,12 @@ Rails.application.configure do
   
   config.serve_static_assets = true
 
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { :host => 'chanceport-main.herokuapp.com' }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-      :address => 'smtp.gmail.com',
-      :port => 587,
-      :domain => 'smtp.gmail.com',
-      :user_name => ENV["GMAIL_ADRESS"],
-      :password => ENV["GMAIL_PASSWORD"],
-      :authentication => :plain,
-      :enable_starttls_auto => true
+  ActionMailer::Base.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    user_name: ENV["GMAIL_ADRESS"],
+    password: ENV["GMAIL_PASSWORD"],
+    authentication: 'plain',
+    enable_starttls_auto: true
   }
 end
